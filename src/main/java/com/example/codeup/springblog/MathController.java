adef7c9224b2e573e9079f7d7b8d9928054fb2b8
Nia Watson <nia.d.watson@gmail.com>
2022-07-27 09:50:40 -0500

    add Home and Post Controllers

---
+++ src/main/java/com/example/codeup/springblog/MathController.java
@@ -0,0 +1,5 @@
+package com.example.codeup.springblog;
+
+public class MathController {
+
+}
---
+++ src/main/java/com/example/codeup/springblog/PostController.java
@@ -0,0 +1,38 @@
+package com.example.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String postsId(@PathVariable int id) {
+        return id + "view an individual post";
+    }
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String postsCreate() {
+        return "view the form for creating a post";
+    }
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String newPost() {
+        return "create a new post";
+    }
+
+
+
+
+
+
+}
