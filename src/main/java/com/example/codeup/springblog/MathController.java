1a28b571fe8db9c98c0b341dd51449bcad5ddf05
Nia Watson <nia.d.watson@gmail.com>
2022-07-27 10:22:06 -0500

    Changes made

--- src/main/java/com/example/codeup/springblog/MathController.java
+++ src/main/java/com/example/codeup/springblog/MathController.java
@@ -1,5 +1,22 @@
 package com.example.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MathController {
+
+//    /add/3/and/4	7
+//            /subtract/3/from/10	7
+//            /multiply/4/and/5	20
+//            /divide/6/by/3	2
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public int add(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + num2;
+    }
 
 }
