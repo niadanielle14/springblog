8280c0b0003a0a42a9b56e5bab886cc8d533d54e
Nia Watson <nia.d.watson@gmail.com>
2022-08-02 20:05:34 -0500

    trying to complete exercise but getting error

--- src/main/java/com/example/codeup/springblog/controller/DiceController.java
+++ src/main/java/com/example/codeup/springblog/controller/DiceController.java
@@ -1,4 +1,26 @@
 package com.example.codeup.springblog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Random;
+@Controller
 public class DiceController {
+    @GetMapping("/roll-dice")
+    public String diceRollGuess() {
+        return "roll-dice";
+    }
+
+    @PostMapping("/roll-dice/{guess}")
+    public String diceRollResults(@PathVariable int guess, Model vModel) {
+        Random rn = new Random();
+        int roll = rn.nextInt(6) + 1;
+        vModel.addAttribute("roll", roll);
+        vModel.addAttribute("guess", guess);
+        return "roll-results";
+    }
 }
+
--- src/main/java/com/example/codeup/springblog/controller/ExampleViewController.java
+++ src/main/java/com/example/codeup/springblog/controller/ExampleViewController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.nio.file.FileStore;
 
 @Controller
 public class ExampleViewController {
